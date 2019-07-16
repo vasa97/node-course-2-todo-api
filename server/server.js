ba26b8a3932a44d5db432704e3735989f1d2116e
vasa97 <49680101+vasa97@users.noreply.github.com>
2019-07-16 16:10:47 +0430

    add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 
 
@@ -70,7 +71,30 @@ app.delete('/todos/:id',(req,res)=>{
     });
 });
 
+//update todo items
+app.patch('/todos/:id',(req,res)=>{
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text','completed']);
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
 
+    if(_.isBoolean(body.completed) && body.completed){
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id,{$set: body},{new:true}).then((todo)=>{
+        if(!todo){
+            res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((err)=>{
+        res.status(400).send();
+    });
+});
 
 
 app.listen(port,()=>{
