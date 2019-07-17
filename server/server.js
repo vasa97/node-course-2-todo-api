6573d476f632b78343cf393ab783ab1819f29a29
vasa97 <49680101+vasa97@users.noreply.github.com>
2019-07-17 19:46:18 +0430

    add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -98,6 +98,19 @@ app.patch('/todos/:id',(req,res)=>{
     });
 });
 
+app.post('/users',(req,res)=>{
+    var body = _.pick(req.body, ['email','password']);
+    var user = new User(body);
+
+    user.save().then(()=>{
+        return user.generateAuthToken();
+    }).then((token)=>{
+        res.header('x-auth',token).send(user);
+    }).catch((e)=> {
+        res.send(e).status(400);
+    });
+});
+
 
 app.listen(port,()=>{
     console.log(`Started on port ${port}`);
