8b56aaa634c61d88416cab32e023a8cfb13cc3bc
vasa97 <49680101+vasa97@users.noreply.github.com>
2019-07-17 21:39:16 +0430

    hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,31 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+var password = '123avc';
+bcrypt.genSalt(10,(err,salt)=>{
+    bcrypt.hash(password,salt,(err,hash)=>{
+        console.log(hash);
+    });
+}); 
+
+var hashedPassword = '$2a$10$fSl1JX3WRlKQjs8hy6USxukKPVyoO1EPjxRrKYRwjLW.haXmZGWQe';
+
+bcrypt.compare(password,hashedPassword,(err,res)=>{
+    console.log(res);
+});
+ 
+// var data = {
+//     id: 10
+// }
+
+// var token = jwt.sign(data,'123abc');
+// console.log(token);
+// var decoded = jwt.verify(token,'123abc');
+// console.log('decoded: ',decoded);
 
 
-var data = {
-    id: 10
-}
 
-var token = jwt.sign(data,'123abc');
-console.log(token);
-var decoded = jwt.verify(token,'123abc');
-console.log('decoded: ',decoded);
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
 
